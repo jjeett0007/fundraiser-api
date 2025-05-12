fde8b3e256b04708805fced33739778327c41173
unknown <User@DESKTOP-6Q1P3TB.cybowpoint.com>
2025-05-12 17:15:06 +0100

    update

---
+++ service/fundraiseService/getAllDonation.js
@@ -0,0 +1,12 @@
+const { FundRaiseDonor } = require("../../model/index");
+const getPaginatedData = require("../../utils/paginationQueries");
+
+const getDonations = async ({ }) => {
+    const filter = {}
+
+    try { } catch (error) {
+        return error
+    }
+}
+
+module.exports = getDonations
